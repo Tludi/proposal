eb9cebe282f9f6552382f27a4647e6aebed4f010
Tludi <timothyludi@diligentagility.com>
2019-02-14 13:12:20 -0800

    Initial Commit

---
+++ proposal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proposal
+//
+//  Created by Timothy Ludi on 2/14/19.
+//  Copyright © 2019 Timothy Ludi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
